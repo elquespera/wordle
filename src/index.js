4a4443fc2f22b5c251cd0f494a785a9a64f79326
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-05-30 05:21:46 +0200

    fix: help modal window

--- src/index.js
+++ src/index.js
@@ -1,10 +1,4 @@
 import './styles/main.scss';
-// import helpIcon from './assets/help.svg';
-// import githubIcon from './assets/github.svg';
-// import chartIcon from './assets/chart.svg';
-// import closeIcon from './assets/close.svg';
-// import returnIcon from './assets/arrow-turn-down.svg';
-// import backspaceIcon from './assets/backspace.svg';
 import puzzle from './puzzle';
 import keyboard from './keyboard';
 import { initLanguage } from './language';
--- src/language.js
+++ src/language.js
@@ -2,7 +2,34 @@ const layouts = {
     en: {
         locale: 'en',
         name: 'English',
-        keys: ['qwertyuiop', 'asdfghjkl', 'zxcvbnm']
+        keys: ['qwertyuiop', 'asdfghjkl', 'zxcvbnm'],
+        help: {
+            title: 'How to play',
+            desc: [
+                `Guess the <strong>WORDLE</strong> in six tries`,
+                `Each guess must be a valid five-letter word. Hit the enter button to submit.`,
+                `After each guess, the color of the tiles will change to show how close your guess was to the word.`
+            ],
+            examplesTitle: 'Examples',
+            examples: {
+                correct: {
+                    word: 'space',
+                    highlight: 0,
+                    msg: `The letter <strong>S</strong> is in the word and in the correct spot.`                    
+                },
+                present: {
+                    word: 'abide',
+                    highlight: 2,
+                    msg: `The letter <strong>I</strong> is in the word but in the wrong spot.`
+                },
+                'not-present': {
+                    word: 'wrong',
+                    highlight: 4,
+                    msg: `The letter <strong>N</strong> is not in the word in any spot.`
+                } 
+            },
+            enjoy: 'Enjoy the game!'
+        }
     },
     es: {
         locale: 'es',
@@ -16,35 +43,39 @@ const layouts = {
     },
 }
 
-// const langBtn = document.querySelector('.lang-btn');
-// const langFlyout = document.querySelector('.lang-flyout');
-
-// let flyoutOpen = false;
 
 const switchLanguage = (layout = layouts.en) => {  
+    const createEl = (tag, text = '', className = '') => {
+        const el = document.createElement(tag);
+        el.innerHTML = text;
+        el.className = className;
+        return el;
+    }
+    const helpPane = document.querySelector('#help');
+    const helpFrag = new DocumentFragment();
     
+    helpFrag.append(createEl('h3', layout.help.title), 
+                    ...layout.help.desc.map(d => createEl('p', d)),
+                    createEl('div', '', 'hr'),
+                    createEl('h4', layout.help.examplesTitle));
+
+    Object.values(layout.help.examples).forEach((example, ind) => {
+        const item = createEl('div', '', 'puzzle');
+        example.word.split('').forEach((letter, i) => {
+            const card = createEl('div', letter, 'card');
+            if (i === example.highlight) {
+                card.classList.add(Object.keys(layout.help.examples)[ind]);
+            }
+            item.appendChild(card);
+        });
+        helpFrag.append(item, createEl('p', example.msg));
+    });
+    helpFrag.append(createEl('div', '', 'hr'), createEl('h4', layout.help.enjoy));
+    helpPane.replaceChildren(helpFrag);
 }
 
 const initLanguage = (layout = layouts.en) => {
-    // const langFrag = new DocumentFragment();
-    // Object.values(layouts).forEach(layout => {
-    //     const item = document.createElement('li');
-    //     item.innerHTML = layout.name;
-    //     langFrag.appendChild(item);
-    // });
-    // langFlyout.replaceChildren(langFrag);
-    // langBtn.addEventListener('click', toggleFlyout);
 
-    // window.addEventListener('click', (event) => {
-    //     if (flyoutOpen && event.target !== langFlyout) {
-    //         toggleFlyout();
-    //     }
-    // });
-    // window.addEventListener('keydown', event => {
-    //     if (flyoutOpen && event.key == 'Escape') {
-    //         toggleFlyout();
-    //     }
-    // });     
 }
 
 export {layouts, initLanguage, switchLanguage};
